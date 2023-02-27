9abf3db10ee8f6a4efc57469e7d10c8b44aaa968
shamitomita <shamitomita@gmail.com>
2023-02-27 17:38:04 -0500

    oops

---
+++ app/controllers/project_controller.rb
@@ -0,0 +1,35 @@
+class ProjectController < ApplicationController
+  before_action :set_project, only: %i[show, destroy]
+
+  def index
+    projects = Project.all.order(created_at: :desc)
+    render json: projects
+  end
+
+  def create
+    project = Project.create!(project_params)
+    if project 
+        render json: project 
+    else 
+      render json: project.errors
+    end 
+  end
+
+  def show
+    render json: @project
+  end
+
+  def destroy
+    @project&.destroy
+    render json: { message: 'Project deleted'} 
+  end
+
+  private 
+  def project_params
+    params.permit(:title, :description, :images, :links)
+  end 
+
+  def set_project 
+    @project = Project.find(params[:id])
+  end 
+end
