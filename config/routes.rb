96da7f7c4f976964d81fe1466138d23ce382adc6
shamitomita <shamitomita@gmail.com>
2023-02-27 17:37:40 -0500

    project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  post 'projects/create'
+  get '/show/:id', to: 'projects#show'
+  delete '/destroy/:id', to: 'projects#destroy'
   root "index#home"
+  get '/*path' => 'index#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
