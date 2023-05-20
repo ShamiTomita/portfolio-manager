e302e198f3f5db62b6d638923cb3ead5250490a7
Shami Tomita <shamitomita@gmail.com>
2023-05-20 16:38:10 -0400

    awards

--- app/javascript/src/components/Awards/Awards.jsx
+++ app/javascript/src/components/Awards/Awards.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import './Awards.css'
 function Awards() {
   return (
-    <div id="awards">Awards</div>
+      <div className='awards' id='awards'>
+        <div className='container'>
+          <div className='col-2'>
+            <h2>Awards</h2>
+            <span className='line'></span>
+            <p>Awards</p>
+            <button className='button'>Explore More</button>
+          </div>
+        </div>
+      </div>
   )
 }
 
