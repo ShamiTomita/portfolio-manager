8cf3ff39793647af5238dcd959b4fc46a48ceb68
Shami Tomita <shamitomita@gmail.com>
2023-05-24 19:51:22 -0400

    about

--- app/javascript/src/components/About/About.jsx
+++ app/javascript/src/components/About/About.jsx
@@ -12,11 +12,25 @@ const About = () => {
         <div className='col-2'>
           <h2>About</h2>
           <span className='line'></span>
-          <div>
+          <div className='blurb'>
               <p>Lorem Ipsums</p>
           </div>
-          <div>
-            
+          <div className='links'>
+            <ul>
+              <li>
+                School
+              </li>
+              <li>
+                Experience
+              </li>
+            </ul>
+          </div>
+          <div className='awards'>
+            <ul>
+              <li>
+                Opportunity Scholar
+              </li>
+            </ul>
           </div>
           <button className='button'>Explore More</button>
         </div>
