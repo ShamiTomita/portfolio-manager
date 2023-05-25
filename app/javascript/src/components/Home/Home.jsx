5bc8b1645372980c868103b210197d9f829a9572
Shami Tomita <shamitomita@gmail.com>
2023-05-25 10:48:00 -0400

    home

--- app/javascript/src/components/Home/Home.jsx
+++ app/javascript/src/components/Home/Home.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNav } from '../../custonHooks/useNav'
 import './Home.css'
+import Selection from './Selection'
 
 const Home = () => {
     const homeRef = useNav('Home')
+
+    const skills = (
+        <div className='change'>
+            <div>
+                <h2>Skills</h2>
+            </div>
+            <div className='categories'>
+                <div>
+                    <h4>Langauges</h4>
+                    <ul>
+                        <li>Ruby</li>
+                        <li>JavaScript</li>
+                        <li>HTML&CSS</li>
+                        <li>RSpec</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Frameworks</h4>
+                    <ul>
+                        <li>Ruby On Rails</li>
+                        <li>React</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Databases</h4>
+                    <ul>
+                        <li>MySQL</li>
+                        <li>PostgreSQL</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+    const resume = (
+        <div className='change'>
+            <div>
+                <h2>Resume</h2>
+            </div>
+            <div className='categories'>
+                <div>
+                    <h4>Langauges</h4>
+                    <ul>
+                        <li>Ruby</li>
+                        <li>JavaScript</li>
+                        <li>HTML&CSS</li>
+                        <li>RSpec</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Frameworks</h4>
+                    <ul>
+                        <li>Ruby On Rails</li>
+                        <li>React</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Databases</h4>
+                    <ul>
+                        <li>MySQL</li>
+                        <li>PostgreSQL</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+    const media = ( 
+        <div className='change'>
+            <div>
+                <h2>Media</h2>
+            </div>
+            <div className='categories'>
+                <div>
+                    <h4>Langauges</h4>
+                    <ul>
+                        <li>Ruby</li>
+                        <li>JavaScript</li>
+                        <li>HTML&CSS</li>
+                        <li>RSpec</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Frameworks</h4>
+                    <ul>
+                        <li>Ruby On Rails</li>
+                        <li>React</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Databases</h4>
+                    <ul>
+                        <li>MySQL</li>
+                        <li>PostgreSQL</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+    const [selection, setSelection] = useState(skills)
+
+   /*  useEffect(() => {
+        setSelection(skills)
+    }, []) */
+    const handleClick = (e) => {
+        e.preventDefault()
+        console.log(e.target.value)
+    }
     return (
         <div ref={homeRef} className='home' id='home'>
             <div className='container'>
@@ -19,41 +126,12 @@ const Home = () => {
                     </div>
                     <br />
                     <div className='links'>
-                        <div><h3>Resume -</h3></div>
-                        <div><h3>- LinkedIn -</h3></div>
-                        <div><h3>- Github </h3></div>
+                        <div><h3 value="skills" onClick={e => handleClick(e)}>Skills</h3></div>
+                        <div><h3 value="resume" onClick={e => handleClick(e)}>Resume</h3></div>
+                        <div><h3 value="media" onClick={e => handleClick(e)}>Media</h3></div>
                     </div>
                 </div>
-                <div className='skills'>
-                    <div>
-                        <h2>Skills</h2>
-                    </div>
-                    <div className='categories'>
-                        <div>
-                            <h4>Langauges</h4>
-                            <ul>
-                                <li>Ruby</li>
-                                <li>JavaScript</li>
-                                <li>HTML&CSS</li>
-                                <li>RSpec</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h4>Frameworks</h4>
-                            <ul>
-                                <li>Ruby On Rails</li>
-                                <li>React</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h4>Databases</h4>
-                            <ul>
-                                <li>MySQL</li>
-                                <li>PostgreSQL</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                    <Selection props={selection}/>
             </div>
         </div>
     )
